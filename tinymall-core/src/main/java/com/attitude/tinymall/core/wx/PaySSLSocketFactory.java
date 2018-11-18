0d17732bbfe2e74a87203f48674423d0f2d2edb7
zhuo1359 <914110393@qq.com>
2018-11-18 22:14:52 +0800

    微信支付_微信退款接口请求与结果接收

---
+++ tinymall-core/src/main/java/com/attitude/tinymall/core/wx/PaySSLSocketFactory.java
@@ -0,0 +1,47 @@
+package com.attitude.tinymall.core.wx;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import com.attitude.tinymall.core.wx.TrustAnyTrustManager;
+
+public class PaySSLSocketFactory extends SSLSocketFactory{
+    static{
+        mySSLSocketFactory = new PaySSLSocketFactory(createSContext());
+    }
+
+    private static PaySSLSocketFactory mySSLSocketFactory = null;
+
+
+
+    private static SSLContext createSContext(){
+        SSLContext sslcontext = null;
+        try {
+            sslcontext = SSLContext.getInstance("SSL");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        try {
+            sslcontext.init(null, new TrustManager[]{new TrustAnyTrustManager()}, null);
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return sslcontext;
+    }
+
+    private PaySSLSocketFactory(SSLContext sslContext) {
+        super(sslContext);
+        this.setHostnameVerifier(ALLOW_ALL_HOSTNAME_VERIFIER);
+    }
+
+    public static PaySSLSocketFactory getInstance(){
+        if(mySSLSocketFactory != null){
+            return mySSLSocketFactory;
+        }else{
+            return mySSLSocketFactory = new PaySSLSocketFactory(createSContext());
+        }
+    }
+}
