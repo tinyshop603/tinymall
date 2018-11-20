7dbb6f2492fd1c496edc8d5628a2d470260c7621
zhuo1359 <914110393@qq.com>
2018-11-20 22:51:00 +0800

    新填商品——添加购物车等流程

--- tinymall-admin-api/src/main/java/com/attitude/tinymall/admin/web/AdminGoodsController.java
+++ tinymall-admin-api/src/main/java/com/attitude/tinymall/admin/web/AdminGoodsController.java
@@ -1,5 +1,9 @@
 package com.attitude.tinymall.admin.web;
 
+import com.attitude.tinymall.db.domain.LitemallGoodsSpecification;
+import com.attitude.tinymall.db.domain.LitemallProduct;
+import com.attitude.tinymall.db.service.LitemallGoodsSpecificationService;
+import com.attitude.tinymall.db.service.LitemallProductService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import com.attitude.tinymall.admin.annotation.LoginAdmin;
@@ -9,6 +13,9 @@ import com.attitude.tinymall.core.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,6 +27,12 @@ public class AdminGoodsController {
 
     @Autowired
     private LitemallGoodsService goodsService;
+
+    @Autowired
+    private LitemallProductService productService;
+
+    @Autowired
+    private LitemallGoodsSpecificationService goodsSpecificationService;
 
     @GetMapping("/list")
     public Object list(@LoginAdmin Integer adminId,
@@ -45,8 +58,53 @@ public class AdminGoodsController {
         if(adminId == null){
             return ResponseUtil.unlogin();
         }
+        goods.setKeywords(goods.getName());
+        goods.setCounterPrice(goods.getRetailPrice());
+        goods.setListPicUrl(goods.getPrimaryPicUrl());
         goodsService.add(goods);
+        if (goods.getId() == null) {
+            return ResponseUtil.badArgument();
+        }
+        syncGoodsSpecification(goods.getId());
+        syncProduct(goods.getId());
         return ResponseUtil.ok(goods);
+    }
+
+    /**
+     * wz-2018-11-20
+     * 添加商品同时添加规格
+     * */
+    private void syncGoodsSpecification(Integer goodsId){
+        LitemallGoodsSpecification goodsSpecification = new LitemallGoodsSpecification();
+        LitemallGoods goods = goodsService.findById(goodsId);
+        goodsSpecification.setGoodsId(goods.getId());
+        goodsSpecification.setValue("标准");
+        goodsSpecification.setPicUrl(goods.getListPicUrl());
+        goodsSpecification.setSpecification("规格");
+        goodsSpecification.setAddTime(LocalDateTime.now());
+        goodsSpecificationService.add(goodsSpecification);
+    }
+
+    /**
+     * wz-2018-11-20
+     * 添加商品同时添加货品
+     * */
+    private void syncProduct(Integer goodsId){
+        LitemallGoods goods = goodsService.findById(goodsId);
+
+        List<LitemallProduct> productList = productService.queryByGid(goodsId);
+
+        Integer[] goodsSpecificationIds = goodsSpecificationService.queryIdsByGid(goodsId);
+
+        LitemallProduct product = new LitemallProduct();
+        product.setGoodsId(goodsId);
+        product.setGoodsNumber(100);
+        product.setRetailPrice(goods.getRetailPrice());
+        product.setGoodsSpecificationIds(goodsSpecificationIds);
+        product.setUrl(goods.getListPicUrl());
+        product.setAddTime(LocalDateTime.now());
+        productService.add(product);
+
     }
 
     @GetMapping("/read")
