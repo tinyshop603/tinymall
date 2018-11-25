c76f6005a8945f2b90089b7e4f38d6a27a278d9b
yangsong <1018761668@qq.com>
2018-11-25 16:33:06 +0800

    add by yangsong 20181012

--- tinymall-admin-api/src/main/java/com/attitude/tinymall/admin/web/AdminGoodsController.java
+++ tinymall-admin-api/src/main/java/com/attitude/tinymall/admin/web/AdminGoodsController.java
@@ -1,5 +1,7 @@
 package com.attitude.tinymall.admin.web;
 
+import com.attitude.tinymall.db.domain.LitemallCategory;
+import com.attitude.tinymall.db.service.LitemallCategoryService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import com.attitude.tinymall.admin.annotation.LoginAdmin;
@@ -9,6 +11,7 @@ import com.attitude.tinymall.core.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,6 +23,8 @@ public class AdminGoodsController {
 
     @Autowired
     private LitemallGoodsService goodsService;
+    @Autowired
+    private LitemallCategoryService categoryService;
 
     @GetMapping("/list")
     public Object list(@LoginAdmin Integer adminId,
@@ -32,11 +37,26 @@ public class AdminGoodsController {
         }
         // 将该用户下的所有的category全部抓取出来,查询所有的商品是该用户的
         List<LitemallGoods> goodsList = goodsService.listGoodsByAdminId(adminId,goodsSn, name,categoryId, page, limit, sort, order);
+        List<LitemallCategory> categoryList = categoryService.getAdminMallCategoryByAdminId(adminId,0, 0, sort, order);
+        String categoryName = null;
+        Integer categoryId2 = 0;
+        Map map = new HashMap();
+        for(int i =0;i<categoryList.size();i++){
+            categoryName = categoryList.get(i).getName();
+            categoryId2 = categoryList.get(i).getId();
+            if(!map.containsKey(categoryId)){
+                map.put(categoryId2,categoryName);
+            }
+        }
+         for(int j = 0;j<goodsList.size();j++){
+            goodsList.get(j).setcategoryName(((String) map.get(goodsList.get(j).getCategoryId())));
+        }
         int total = goodsService.countGoodsByAdminId(adminId,goodsSn, name, categoryId);
+
         Map<String, Object> data = new HashMap<>();
         data.put("total", total);
         data.put("items", goodsList);
-
+        data.put("categoryMap", map);
         return ResponseUtil.ok(data);
     }
 
@@ -45,6 +65,7 @@ public class AdminGoodsController {
         if(adminId == null){
             return ResponseUtil.unlogin();
         }
+      //  goods.setCategoryId(Integer.parseInt(goods.getcategoryName()));
         goodsService.add(goods);
         return ResponseUtil.ok(goods);
     }
@@ -68,6 +89,7 @@ public class AdminGoodsController {
         if(adminId == null){
             return ResponseUtil.unlogin();
         }
+      //  goods.setCategoryId(Integer.parseInt(goods.getcategoryName()));
         goodsService.updateById(goods);
         return ResponseUtil.ok(goods);
     }
