a035ca044aaabfad3fdffefef1d513974bf48080
zhuo1359 <914110393@qq.com>
2018-12-20 00:52:00 +0800

    # 小程序确认收货后 商家订单实时更新

--- tinymall-core/src/main/java/com/attitude/tinymall/core/common/SocketEvent.java
+++ tinymall-core/src/main/java/com/attitude/tinymall/core/common/SocketEvent.java
@@ -9,4 +9,5 @@ public final class SocketEvent {
   public static final String SUBMIT_ORDER = "submitOrderEvent";
   public static final String CANCEL_ORDER = "cancelOrderEvent";
   public static final String REFUND_ORDER = "refundOrderEvent";
+  public static final String CONFIRM_ORDER = "confirmOrderEvent";
 }
