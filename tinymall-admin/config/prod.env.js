013e6b864f0281d7dd9223d167ae12dd22e5e5c5
zhaoguiyang <guiyang.zhao@advance.ai>
2018-12-25 00:56:31 +0800

    fix config bug

--- tinymall-admin/config/prod.env.js
+++ tinymall-admin/config/prod.env.js
@@ -4,6 +4,6 @@ module.exports = {
   BASE_API: '"https://www.bjguangchi.top/admin"',
   OS_API: '"https://www.bjguangchi.top/os"',
   SOCKET_URL: '"https://www.bjguangchi.top"',
-  LODOP_PLUGIN_SRC1_URL: '"http://localhost:8443"',
-  LODOP_PLUGIN_SRC2_URL: '"http://localhost:8444"'
+  LODOP_PLUGIN_SRC1_URL: '"https://localhost:8443"',
+  LODOP_PLUGIN_SRC2_URL: '"https://localhost:8444"'
 }
