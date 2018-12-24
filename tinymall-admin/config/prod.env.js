6c91ccd222b4c6cad1733bd4730bce1a98664187
zhaoguiyang <zhaoguiyang618@gmail.com>
2018-12-25 00:45:12 +0800

    prefer the printer function

--- tinymall-admin/config/prod.env.js
+++ tinymall-admin/config/prod.env.js
@@ -1,7 +1,9 @@
 module.exports = {
-	NODE_ENV: '"production"',
-	ENV_CONFIG: '"prod"',
-  	BASE_API: '"https://www.bjguangchi.top/admin"',
-  	OS_API: '"https://www.bjguangchi.top/os"',
-  	SOCKET_URL: '"https://www.bjguangchi.top"'
+  NODE_ENV: '"production"',
+  ENV_CONFIG: '"prod"',
+  BASE_API: '"https://www.bjguangchi.top/admin"',
+  OS_API: '"https://www.bjguangchi.top/os"',
+  SOCKET_URL: '"https://www.bjguangchi.top"',
+  LODOP_PLUGIN_SRC1_URL: '"http://localhost:8443"',
+  LODOP_PLUGIN_SRC2_URL: '"http://localhost:8444"'
 }
