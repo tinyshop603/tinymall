16514412850ecf2f96de7e5955b7a9afd699141f
zhaoguiyang <guiyang.zhao@advance.ai>
2019-05-17 08:45:02 +0800

    add order status

---
+++ tinymall-db/src/main/java/com/attitude/tinymall/db/enums/PaymentModelEnum.java
@@ -0,0 +1,23 @@
+package com.attitude.tinymall.db.enums;
+
+/**
+ * @author zhaoguiyang on 2019/5/16.
+ * @project Wechat
+ */
+public enum  PaymentModelEnum {
+
+  /**
+   * 线上微信支付
+   */
+  ONLINE_WECHAT_PAY,
+
+  /**
+   * 线上支付宝支付
+   */
+  ONLINE_ALI_PAY,
+
+  /**
+   * 货到付款
+   */
+  CASH_ON_DELIVERY
+}
