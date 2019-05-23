dfca686f0d743283458428d929992f2d33e2d766
zhaoguiyang <guiyang.zhao@advance.ai>
2019-05-23 09:06:51 +0800

    add service

---
+++ tinymall-db/src/main/java/com/attitude/tinymall/service/LitemallDeliveryDetailService.java
@@ -0,0 +1,9 @@
+package com.attitude.tinymall.service;
+
+/**
+ * @author zhaoguiyang on 2019/5/23.
+ * @project Wechat
+ */
+public interface LitemallDeliveryDetailService {
+
+}
---
+++ tinymall-db/src/main/java/com/attitude/tinymall/service/impl/LitemallDeliveryDetailServiceImpl.java
@@ -0,0 +1,11 @@
+package com.attitude.tinymall.service.impl;
+
+import com.attitude.tinymall.service.LitemallDeliveryDetailService;
+
+/**
+ * @author zhaoguiyang on 2019/5/23.
+ * @project Wechat
+ */
+public class LitemallDeliveryDetailServiceImpl implements LitemallDeliveryDetailService {
+
+}
