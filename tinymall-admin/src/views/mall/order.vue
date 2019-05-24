cd5cdcd5cc2f5cf276c0d9f37344ede8d996bc77
zhaoguiyang <zhaoguiyang618@gmail.com>
2019-05-24 09:13:23 +0800

    add order vue option

--- tinymall-admin/src/views/mall/order.vue
+++ tinymall-admin/src/views/mall/order.vue
@@ -2,20 +2,27 @@
   <div class="app-container calendar-list-container">
     <!-- 查询和其他操作 -->
     <div class="filter-container">
-      <el-input clearable class="filter-item" style="width: 200px;" placeholder="请输入用户ID" v-model="listQuery.userId"></el-input>
-      <el-input clearable class="filter-item" style="width: 200px;" placeholder="请输入订单编号" v-model="listQuery.orderSn"></el-input>
+      <el-input clearable class="filter-item" style="width: 200px;" placeholder="请输入用户ID"
+                v-model="listQuery.userId"></el-input>
+      <el-input clearable class="filter-item" style="width: 200px;" placeholder="请输入订单编号"
+                v-model="listQuery.orderSn"></el-input>
       <el-button class="filter-item" type="primary" v-waves icon="el-icon-search" @click="handleFilter">查找</el-button>
-      <el-button class="filter-item" type="primary" v-waves icon="el-icon-download" @click="handleDownload" :loading="downloadLoading">导出订单信息</el-button>
+      <el-button class="filter-item" type="primary" v-waves icon="el-icon-download" @click="handleDownload"
+                 :loading="downloadLoading">导出订单信息
+      </el-button>
     </div>
     <!-- 查询结果 -->
-    <el-table size="small" :data="list" v-loading="listLoading" element-loading-text="正在查询中。。。" border fit highlight-current-row>
+    <el-table size="small" :data="list" v-loading="listLoading" element-loading-text="正在查询中。。。" border fit
+              highlight-current-row>
       <el-table-column type="expand">
         <template slot-scope="props">
-          <el-table size="small" :data="props.row.goods" v-loading="listLoading" element-loading-text="正在查询中。。。" border fit highlight-current-row>
+          <el-table size="small" :data="props.row.goods" v-loading="listLoading" element-loading-text="正在查询中。。。" border
+                    fit highlight-current-row>
             <el-table-column align="center" min-width="90px" label="商品条形码" prop="goodsSn"></el-table-column>
             <el-table-column align="center" min-width="90px" label="商品名称" prop="goodsName"></el-table-column>
             <el-table-column align="center" min-width="90px" label="商品数量" prop="number"></el-table-column>
-            <el-table-column align="center" min-width="90px" label="商品规格" prop="goodsSpecificationValues"></el-table-column>
+            <el-table-column align="center" min-width="90px" label="商品规格"
+                             prop="goodsSpecificationValues"></el-table-column>
             <el-table-column align="center" min-width="90px" label="商品单价" prop="retailPrice"></el-table-column>
           </el-table>
         </template>
@@ -37,35 +44,38 @@
       <el-table-column align="center" width="80px" label="订单费用" prop="order.orderPrice"></el-table-column>
       <el-table-column align="center" min-width="80px" label="支付方式" prop="order.paymentWay">
         <template slot-scope="scope">
-          <el-tag :type="scope.row.order.paymentWay ? 'success' : 'error' ">{{scope.row.order.paymentWay==1 ? '在线支付' : '货到付款'}}</el-tag>
+          <el-tag :type="scope.row.order.paymentWay ? 'success' : 'error' ">{{scope.row.order.paymentWay==1 ? '在线支付' :
+            '货到付款'}}
+          </el-tag>
         </template>
       </el-table-column>
       <!-- TODO 订单详情页的修改, 点击弹出订单的详情-->
       <el-table-column align="center" min-width="80px" label="订单详情" prop="order.remark"></el-table-column>
       <el-table-column align="center" label="操作" width="370" class-name="small-padding fixed-width" prop="type">
         <template slot-scope="scope">
-          <el-dropdown>
+          <el-dropdown trigger="click" @command="handleOrderOptionCommand">
             <el-button type="primary">
-              更多菜单<i class="el-icon-arrow-down el-icon--right"></i>
+              订单操作<i class="el-icon-arrow-down el-icon--right"></i>
             </el-button>
             <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item>黄金糕</el-dropdown-item>
-              <el-dropdown-item>狮子头</el-dropdown-item>
-              <el-dropdown-item>螺蛳粉</el-dropdown-item>
-              <el-dropdown-item>双皮奶</el-dropdown-item>
-              <el-dropdown-item>蚵仔煎</el-dropdown-item>
+              <el-dropdown-item :command='{type:"acceptOrder", itemData:scope.row.order}'>接受订单</el-dropdown-item>
+              <el-dropdown-item :command='{type:"callRider", itemData:scope.row.order}'>呼叫骑手</el-dropdown-item>
+              <el-dropdown-item :command='{type:"cancelOrder", itemData:scope.row.order}'>取消订单</el-dropdown-item>
+              <el-dropdown-item :command='{type:"confirmRefund", itemData:scope.row.order}'>确认退款</el-dropdown-item>
             </el-dropdown-menu>
           </el-dropdown>
-          <el-button :type="scope.row.sendBtnStatus.type" :disabled="scope.row.sendBtnStatus.disabled" size="small" @click="handleSend(scope.row.order)">发货</el-button>
-          <el-button :type="scope.row.cancelBtnStatus.type" :disabled="scope.row.cancelBtnStatus.disabled" size="small" @click="handleCancelOrder(scope.row.order)">取消订单</el-button>
-          <el-button :type="scope.row.confirmBtnStatus.type" :disabled="scope.row.confirmBtnStatus.disabled" size="small" @click="handleConfirmOrder(scope.row.order)">确认完成</el-button>
-          <el-button :type="scope.row.refundBtnStatus.type" :disabled="scope.row.refundBtnStatus.disabled" size="small" @click="handleRefundOrder(scope.row.order)">申请退货</el-button>
+          <!--          <el-button :type="scope.row.sendBtnStatus.type" :disabled="scope.row.sendBtnStatus.disabled" size="small" @click="handleSend(scope.row.order)">发货</el-button>-->
+          <!--          <el-button :type="scope.row.cancelBtnStatus.type" :disabled="scope.row.cancelBtnStatus.disabled" size="small" @click="handleCancelOrder(scope.row.order)">取消订单</el-button>-->
+          <!--          <el-button :type="scope.row.confirmBtnStatus.type" :disabled="scope.row.confirmBtnStatus.disabled" size="small" @click="handleConfirmOrder(scope.row.order)">确认完成</el-button>-->
+          <!--          <el-button :type="scope.row.refundBtnStatus.type" :disabled="scope.row.refundBtnStatus.disabled" size="small" @click="handleRefundOrder(scope.row.order)">申请退货</el-button>-->
         </template>
       </el-table-column>
     </el-table>
     <!-- 分页 -->
     <div class="pagination-container">
-      <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="listQuery.page" :page-sizes="[10,20,30,50]" :page-size="listQuery.limit" layout="total, sizes, prev, pager, next, jumper" :total="total"></el-pagination>
+      <el-pagination background @size-change="handleSizeChange" @current-change="handleCurrentChange"
+                     :current-page="listQuery.page" :page-sizes="[10,20,30,50]" :page-size="listQuery.limit"
+                     layout="total, sizes, prev, pager, next, jumper" :total="total"></el-pagination>
     </div>
     <!-- 发货对话框 -->
     <el-dialog title="发货" :visible.sync="sendDialogFormVisible">
@@ -109,23 +119,31 @@
   </div>
 </template>
 <style>
-.demo-table-expand {
-  font-size: 0;
-}
+  .demo-table-expand {
+    font-size: 0;
+  }
 
-.demo-table-expand label {
-  width: 200px;
-  color: #99a9bf;
-}
+  .demo-table-expand label {
+    width: 200px;
+    color: #99a9bf;
+  }
 
-.demo-table-expand .el-form-item {
-  margin-right: 0;
-  margin-bottom: 0;
-}
+  .demo-table-expand .el-form-item {
+    margin-right: 0;
+    margin-bottom: 0;
+  }
 
 </style>
 <script>
-import { listOrder, getBtnStateByCode, updateOrderCode, STATUS, refundOrder, formatDate, formatDate2 } from '@/api/order'
+import {
+  listOrder,
+  getBtnStateByCode,
+  updateOrderCode,
+  STATUS,
+  refundOrder,
+  formatDate,
+  formatDate2
+} from '@/api/order'
 import waves from '@/directive/waves' // 水波纹指令
 export default {
   name:'Order',
@@ -231,8 +249,25 @@ export default {
       }
     }
   },
-  mounted:function() {},
+  mounted:function() {
+  },
   methods:{
+    handleOrderOptionCommand(command) {
+      switch (command.type) {
+        case 'acceptOrder':
+          this.$message('click on item ' + command.type)
+          break
+        case 'callRider':
+          this.$message('click on item ' + command.type)
+          break
+        case 'cancelOrder':
+          this.$message('click on item ' + command.type)
+          break
+        case 'confirmRefund':
+          this.$message('click on item ' + command.type)
+          break
+      }
+    },
     getList() {
       this.listLoading = true
       listOrder(this.listQuery).then(response => {
@@ -266,6 +301,7 @@ export default {
       this.dataForm.orderStatus = row.orderStatus
     },
     handleSend(row) {
+      debugger
       this.resetForm(row)
       this.sendDialogFormVisible = true
       // this.$nextTick(() => {
@@ -274,7 +310,7 @@ export default {
     },
     sendData() {
       /**
-            this.$refs['dataForm'].validate((valid) => {
+         this.$refs['dataForm'].validate((valid) => {
               if (valid) {
                 updateOrder(this.dataForm).then(response => {
                   const updatedOrder = response.data.data
@@ -295,7 +331,7 @@ export default {
                 })
               }
             })
-            **/
+         **/
       // 更改当前的订单的状态为发货状态
       if (this.dataForm.orderStatus === 201) {
         this.dataForm.orderStatus = STATUS.SHIP // 微信支付
@@ -398,12 +434,12 @@ export default {
     },
     handleDownload() {
       this.downloadLoading = true
-      import('@/vendor/Export2Excel').then(excel => {
-        const tHeader = ['订单ID', '订单编号', '用户ID', '订单状态', '是否删除', '收货人', '收货联系电话', '收货地址']
-        const filterVal = ['id', 'orderSn', 'userId', 'orderStatis', 'isDelete', 'consignee', 'mobile', 'address']
-        excel.export_json_to_excel2(tHeader, this.list, filterVal, '订单信息')
-        this.downloadLoading = false
-      })
+        import('@/vendor/Export2Excel').then(excel => {
+          const tHeader = ['订单ID', '订单编号', '用户ID', '订单状态', '是否删除', '收货人', '收货联系电话', '收货地址']
+          const filterVal = ['id', 'orderSn', 'userId', 'orderStatis', 'isDelete', 'consignee', 'mobile', 'address']
+          excel.export_json_to_excel2(tHeader, this.list, filterVal, '订单信息')
+          this.downloadLoading = false
+        })
     },
     updateOrderItemStatus(updatedOrder) {
       for (const v of this.list) {
