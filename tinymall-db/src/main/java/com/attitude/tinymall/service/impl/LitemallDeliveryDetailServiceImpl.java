1b123ea53d656f64658dca8d450d546c9b3f138e
yangsong <1018761668@qq.com>
2019-05-25 13:43:29 +0800

    add api

--- tinymall-db/src/main/java/com/attitude/tinymall/service/impl/LitemallDeliveryDetailServiceImpl.java
+++ tinymall-db/src/main/java/com/attitude/tinymall/service/impl/LitemallDeliveryDetailServiceImpl.java
@@ -1,7 +1,10 @@
 package com.attitude.tinymall.service.impl;
 
+import com.attitude.tinymall.dao.LitemallDeliveryDetailMapper;
+import com.attitude.tinymall.domain.LitemallDeliveryDetail;
 import com.attitude.tinymall.domain.LitemallOrder;
 import com.attitude.tinymall.domain.LitemallUser;
+import com.attitude.tinymall.domain.baidu.address.Location;
 import com.attitude.tinymall.domain.dada.ResponseEntity;
 import com.attitude.tinymall.domain.dada.order.*;
 import com.attitude.tinymall.service.LitemallAdminService;
@@ -9,6 +12,7 @@ import com.attitude.tinymall.service.LitemallDeliveryDetailService;
 import com.attitude.tinymall.service.LitemallOrderService;
 import com.attitude.tinymall.service.LitemallUserService;
 import com.attitude.tinymall.service.client.RemoteDadaDeliveryClient;
+import com.attitude.tinymall.util.IdGeneratorUtil;
 import com.attitude.tinymall.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,6 +34,11 @@ public class LitemallDeliveryDetailServiceImpl implements LitemallDeliveryDetail
   @Autowired
   LitemallUserService litemalluserService;
 
+  @Autowired
+  BaiduFenceServiceImpl BaiduFenceService;
+
+  @Autowired
+  LitemallDeliveryDetailMapper litemallDeliveryDetailMapper;
 
     /**
      * 新增订单
@@ -39,6 +48,9 @@ public class LitemallDeliveryDetailServiceImpl implements LitemallDeliveryDetail
   public String dadaAddOrder( Integer orderId){
     LitemallOrder order =  litemallOrderService.findById(orderId);
     LitemallUser user = litemalluserService.findById(order.getUserId());
+    Location location = BaiduFenceService.geocoding("order.getAddress()").getLocation();
+
+    order.setDeliveryId(IdGeneratorUtil.generateId("dada"));//TODO
     AddOrderParams  orderParams = AddOrderParams.builder()
             .shopNo("${delivery.dada.source-id}")
             .cityCode("010")//北京
@@ -46,16 +58,20 @@ public class LitemallDeliveryDetailServiceImpl implements LitemallDeliveryDetail
             .isPrepay(0)
             .receiverName(user.getUsername() )
             .receiverAddress(order.getAddress())
-            //    .receiverLat(order.get)
-            //   .receiverLng(new Float("116.3084202915042"))
+            .receiverLat(Float.parseFloat(""+location.getLat()))
+            .receiverLng(Float.parseFloat(""+location.getLng()))
             .receiverPhone(user.getMobile())
-            .originId(orderId.toString())
+            .originId(order.getDeliveryId())
             .callback("http://39.107.81.107:8084/dada-order/callback/status")
             .build();
 
     ResponseEntity<AddOrderResult> res = remoteDadaDeliveryClient.addOrder(orderParams);
+    LitemallDeliveryDetail litemallDeliveryDetail = new LitemallDeliveryDetail();
+    litemallDeliveryDetail.setDeliveryId(order.getDeliveryId());
     if ("0".equals(res.getStatus())) {
       //TODO 插订单配送状态
+//      litemallOrderService.updateById(order);
+//      litemallDeliveryDetailMapper.insert(litemallDeliveryDetail);
     }
     return res.status;
   }
