8c913d7fd467e7f0cb58b39fae0d766e07abbd9d
zhaoguiyang <guiyang.zhao@advance.ai>
2019-05-25 11:10:39 +0800

    add interface

--- tinymall-db/src/main/java/com/attitude/tinymall/domain/LitemallOrderWithGoods.java
+++ tinymall-db/src/main/java/com/attitude/tinymall/domain/LitemallOrderWithGoods.java
@@ -2,12 +2,13 @@ package com.attitude.tinymall.domain;
 
 import java.io.Serializable;
 import java.util.List;
+import lombok.Data;
 
 /**
  * 订单和对应商品的实体类
  */
+@Data
 public class LitemallOrderWithGoods implements Serializable{
-
   /**
    * 订单信息
    */
@@ -17,20 +18,6 @@ public class LitemallOrderWithGoods implements Serializable{
    */
   private List<com.attitude.tinymall.domain.LitemallOrderGoods> goods;
 
+  private LitemallDeliveryDetail deliveryDetail;
 
-  public LitemallOrder getOrder() {
-    return order;
-  }
-
-  public void setOrder(LitemallOrder order) {
-    this.order = order;
-  }
-
-  public List<LitemallOrderGoods> getGoods() {
-    return goods;
-  }
-
-  public void setGoods(List<LitemallOrderGoods> goods) {
-    this.goods = goods;
-  }
 }
--- tinymall-db/src/main/java/com/attitude/tinymall/service/LitemallDeliveryDetailService.java
+++ tinymall-db/src/main/java/com/attitude/tinymall/service/LitemallDeliveryDetailService.java
@@ -1,9 +1,13 @@
 package com.attitude.tinymall.service;
 
+import com.attitude.tinymall.domain.LitemallDeliveryDetail;
+
 /**
  * @author zhaoguiyang on 2019/5/23.
  * @project Wechat
  */
 public interface LitemallDeliveryDetailService {
+
+  LitemallDeliveryDetail getDeliveryDetailByOrderId(String orderId);
 
 }
