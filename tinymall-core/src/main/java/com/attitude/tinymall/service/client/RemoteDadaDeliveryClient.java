b3a07249250a51218c73c8390d1ef9877510d119
yangsong <1018761668@qq.com>
2019-05-25 12:23:03 +0800

    add api

--- tinymall-core/src/main/java/com/attitude/tinymall/service/client/RemoteDadaDeliveryClient.java
+++ tinymall-core/src/main/java/com/attitude/tinymall/service/client/RemoteDadaDeliveryClient.java
@@ -68,7 +68,7 @@ public interface RemoteDadaDeliveryClient {
   ResponseEntity<FormalCancelOrderResult> formalCancelOrder(@RequestBody FormalCancelParams params);
 
   @PostMapping("/api/order/cancel/reasons")
-  ResponseEntity<CancelOrderReasonsResult> cancelOrderReasons();
+  ResponseEntity<List<CancelOrderReasonsResult>> cancelOrderReasons();
 
   @PostMapping("/api/order/appoint/exist")
   ResponseEntity existOrderAppoint(@RequestBody ExistOrderParams params);
@@ -83,7 +83,7 @@ public interface RemoteDadaDeliveryClient {
   ResponseEntity dadaComplaint(@RequestBody DadaParams params);
 
   @PostMapping("/api/complaint/reasons")
-  ResponseEntity<ComplaintReasonsResult> complaintReasons();
+  ResponseEntity<List<ComplaintReasonsResult>> complaintReasons();
 
   @PostMapping("/api/order/confirm/goods")
   ResponseEntity confirmOrderGoods(@RequestBody GoodsParam params);
@@ -92,7 +92,7 @@ public interface RemoteDadaDeliveryClient {
   ResponseEntity<AddShopResult> addShop(@RequestBody List<AddShopParams> shopParams);
 
   @GetMapping("/api/cityCode/list")
-  ResponseEntity<ListCityResult> getCityCodeList();
+  ResponseEntity<List<ListCityResult>> getCityCodeList();
 
   @PostMapping("/merchantApi/merchant/add")
   ResponseEntity addMerchant(@RequestBody MerchantAddParams shopParams);
