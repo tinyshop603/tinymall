b805681e02a3e50f0e6ec66fbb51515c25caf758
zhaoguiyang <guiyang.zhao@advance.ai>
2019-06-01 14:20:56 +0800

    add feiclient

--- tinymall-os-api/src/main/java/com/attitude/tinymall/OsApiApplication.java
+++ tinymall-os-api/src/main/java/com/attitude/tinymall/OsApiApplication.java
@@ -2,8 +2,10 @@ package com.attitude.tinymall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages={"com.attitude.tinymall"})
+@EnableFeignClients(basePackages = "com.attitude.tinymall")
 public class OsApiApplication {
 
     public static void main(String[] args) {
