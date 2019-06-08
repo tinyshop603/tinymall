bf2becdd76b1d0cb416502cdbc215c4b9b4c74ee
zhaoguiyang <guiyang.zhao@advance.ai>
2019-06-08 14:40:34 +0800

    add api

--- tinymall-wx-api/src/main/java/com/attitude/tinymall/web/WxAddressController.java
+++ tinymall-wx-api/src/main/java/com/attitude/tinymall/web/WxAddressController.java
@@ -4,6 +4,7 @@ import com.attitude.tinymall.domain.baidu.address.Location;
 import com.attitude.tinymall.domain.baidu.address.PoiAddress;
 import com.attitude.tinymall.domain.baidu.geocode.GeoDecodingAddress;
 import com.attitude.tinymall.service.BaiduFenceService;
+import com.attitude.tinymall.service.IUserAddressService;
 import com.attitude.tinymall.util.CoodinateCovertorUtil;
 import com.attitude.tinymall.util.ResponseUtil;
 import com.attitude.tinymall.service.LitemallAdminService;
@@ -37,6 +38,8 @@ public class WxAddressController {
     private BaiduFenceService baiduFenceService;
     @Autowired
     private LitemallAdminService adminService;
+    @Autowired
+    private IUserAddressService userAddressService;
 
     /**
      * 用户收货地址列表
@@ -210,39 +213,17 @@ public class WxAddressController {
         return ResponseUtil.ok();
     }
 
-    @GetMapping("location")
-    public Object getLoacationByGeoParams(@LoginUser Integer userId,
-                                          @RequestParam String lng,
+    @GetMapping("/location")
+    public Object getLocationByGeoParams(@RequestParam String lng,
                                           @RequestParam String lat,
                                           String keyword,
                                           @PathVariable("storeId") String appId) {
 
-        Location bd09Location = CoodinateCovertorUtil.gcj02ToBd09(new Location(Double.valueOf(lng), Double.valueOf(lat)));
-        try {
-            GeoDecodingAddress geoDecodingAddress = baiduFenceService.reverseGeocoding(bd09Location);
-            LocationVO locationVO = new LocationVO();
-            BeanUtils.copyProperties(geoDecodingAddress, locationVO);
-            boolean isValidAddress = baiduFenceService
-                    .isValidLocationWithinFence(userId.toString(), geoDecodingAddress.getLocation(),
-                            adminService.findAdminByOwnerId(appId).getShopFenceId());
-            locationVO.setDistributionStatus(isValidAddress ? "in" : "out");
-
-            if (StringUtils.isEmpty(keyword)){
-                locationVO.setKeywordsNearbyAddresses(Collections.emptyList());
-            }else {
-                // TODO region应根据店铺位置来, 目前只是在北京
-                List<PoiAddress> poiAddresses = baiduFenceService.listPlacesByKeywords(keyword, "北京");
-                locationVO.setKeywordsNearbyAddresses(poiAddresses
-                        .stream()
-                        .map(it -> new PoiAddressVO(it.getAddress(), it.getName()))
-                        .collect(Collectors.toList()));
-            }
-
-            return ResponseUtil.ok(locationVO);
-        } catch (Exception e) {
-            e.printStackTrace();
+        LocationVO res = userAddressService.getLocationDetailByGeoParams(lng, lat, keyword, appId);
+        if (res == null){
             return ResponseUtil.fail();
         }
+        return ResponseUtil.ok(res);
     }
 
 }
